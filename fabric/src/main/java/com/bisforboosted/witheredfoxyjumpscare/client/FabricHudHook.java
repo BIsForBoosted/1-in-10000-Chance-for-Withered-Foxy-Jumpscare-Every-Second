237aaae91769973cc4650095cf5ead4f77785dc4
BIsForBoosted <52310387+BIsForBoosted@users.noreply.github.com>
2026-01-06 19:12:08 -0600

    Removed unused identifier

--- fabric/src/main/java/com/bisforboosted/witheredfoxyjumpscare/client/FabricHudHook.java
+++ fabric/src/main/java/com/bisforboosted/witheredfoxyjumpscare/client/FabricHudHook.java
@@ -1,5 +1,6 @@
 package com.bisforboosted.witheredfoxyjumpscare.client;
 
+import com.bisforboosted.witheredfoxyjumpscare.Constants;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.hud.HudElementRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.hud.VanillaHudElements;
@@ -7,6 +8,6 @@ import net.fabricmc.fabric.api.client.rendering.v1.hud.VanillaHudElements;
 public class FabricHudHook implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        HudElementRegistry.attachElementAfter(VanillaHudElements.CROSSHAIR, JumpscareHudRenderer.WITHERED_FOXY_JUMPSCARE, JumpscareHudRenderer::render);
+        HudElementRegistry.attachElementAfter(VanillaHudElements.CROSSHAIR, Constants.WITHERED_FOXY_JUMPSCARE_1, JumpscareHudRenderer::render);
     }
 }
