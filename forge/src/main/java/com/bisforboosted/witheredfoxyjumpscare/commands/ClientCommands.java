ba74002a2fa42e0915d3073cff646d303ed1efba
BIsForBoosted <52310387+BIsForBoosted@users.noreply.github.com>
2026-01-09 17:55:18 -0600

    Added jumpscare command

---
+++ forge/src/main/java/com/bisforboosted/witheredfoxyjumpscare/commands/ClientCommands.java
@@ -0,0 +1,37 @@
+package com.bisforboosted.witheredfoxyjumpscare.commands;
+
+import com.bisforboosted.witheredfoxyjumpscare.Constants;
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.Commands;
+import net.minecraft.network.chat.Component;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.eventbus.api.listener.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = Constants.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
+public class ClientCommands {
+    @SubscribeEvent
+    public static void onRegisterCommands(RegisterCommandsEvent event) {
+        CommandDispatcher<CommandSourceStack> dispatcher = event.getDispatcher();
+
+        dispatcher.register(Commands.literal("setjumpscareodds")
+                .then(Commands.argument("odds", IntegerArgumentType.integer(1))
+                        .executes(context -> {
+                            CommonCommands.CommandOutput output = CommonCommands.changeJumpscareOdds(context);
+                            int result = output.result();
+                            if (result == 0){
+                                context.getSource().sendFailure(Component.literal("Failed to set jumpscare odds"));
+                            } else {
+                                context.getSource().sendSuccess(
+                                        () -> Component.literal("New jumpscare odds set to 1/" + output.odds() + "!"),
+                                        true
+                                );
+                            }
+                            return result;
+                        })
+                ));
+    }
+}
