61d0ad11d6d3016bb9a06ccee0dc61ef06bde819
BIsForBoosted <52310387+BIsForBoosted@users.noreply.github.com>
2026-02-22 13:11:44 -0600

    Converted Fabric hook to 1.20.4

--- fabric/src/main/java/com/bisforboosted/witheredfoxyjumpscare/client/FabricHudHook.java
+++ fabric/src/main/java/com/bisforboosted/witheredfoxyjumpscare/client/FabricHudHook.java
@@ -2,12 +2,13 @@ package com.bisforboosted.witheredfoxyjumpscare.client;
 
 import com.bisforboosted.witheredfoxyjumpscare.Constants;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.rendering.v1.hud.HudElementRegistry;
-import net.fabricmc.fabric.api.client.rendering.v1.hud.VanillaHudElements;
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 
 public class FabricHudHook implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        HudElementRegistry.attachElementAfter(VanillaHudElements.CROSSHAIR, Constants.WITHERED_FOXY_JUMPSCARE_1, JumpscareHudRenderer::render);
+        HudRenderCallback.EVENT.register((context, tickDelta) -> {
+            JumpscareHudRenderer.render(context);
+        });
     }
 }
